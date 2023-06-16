214d880a37a5499523e8ab5155b0c464ecf7d524
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-06-16 17:59:28 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    let messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      messages.push(
+        `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`
+      )
+    }
+  
+    return messages;
+  }
+  
+  function countDown(countdown) {
+    while (countdown >= 0) {
+      countdown--
+      console.log(countdown+1)
+    }
+  }
+  countDown(5)
